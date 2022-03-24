637df35015dcbb3646e5d6f809e5f5e40acc6cc9
lauramjor <101403221+lauramjor@users.noreply.github.com>
2022-03-24 14:23:17 +0100

    added Thursday and Friday

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function displayForecast(){
   let forecastElement = document.querySelector("#forecast");
   
   let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tue", "Wed"];
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri];
   days.forEach(function(day) {
        forecastHTML = forecastHTML +
 				 `<div class="row">
