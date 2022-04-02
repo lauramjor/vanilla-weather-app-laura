7e96d9af02c466de40ab7480ed1d1d39a1d4bcf4
lauramjor <101403221+lauramjor@users.noreply.github.com>
2022-04-02 16:39:59 +0200

    updated displayForecast

--- index.js
+++ index.js
@@ -24,28 +24,30 @@ function formatDay(timestamp){
 	
 }
 
-function displayForecast(response){
+function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function(forecastDay, index) {
-	  if (index < 6){
-	  
-       forecastHTML = forecastHTML +
-				 `<div class="row">
-				  <div class="col-2">
-					<div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-					  <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42">
-					    <div class="weather-forecast-temperatures">
-						<span class="weather-forecast-temperature-max">
-							${Math.round(forecastDay.temp.max)} </span>
-						<span class="weather-forecast-temperature-min">
-							${Math.round(forecastDay.temp.min)} </span>
-					</div>
-		</div>`;}
-});
-   forecastHTML = forecastHTML + `</div>`;
-   forecastElement.innerHTML = forecastHTML;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2 text-center">
+                    <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+                      <img src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png" alt="" width="42">
+                        <div class="weather-forecast-temperatures">
+                        <span class="weather-forecast-temperature-max">
+                            ${Math.round(forecastDay.temp.max)} </span>
+                        <span class="weather-forecast-temperature-min">
+                            ${Math.round(forecastDay.temp.min)} </span>
+                    </div>
+        </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 //Feature 2 - Add a search engine, when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
 function search(event) {
